22ca0c7590e9a7122557de029e8312d4ac1d4ad4
Harshit Jaiswal <harshitjaiswal022@gmail.com>
2025-12-03 22:58:47 +0530

    completed the DashBoard Page UI

--- src/components/TitleCards/TitleCards.jsx
+++ src/components/TitleCards/TitleCards.jsx
@@ -1,7 +1,34 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import "./TitleCards.css";
-const TitleCards = () => {
-  return <div className="titlecards"></div>;
+import cards_data from "../../assets/cards/Cards_data";
+const TitleCards = ({ title, category }) => {
+  const cardRef = useRef();
+  const handlewheel = (event) => {
+    event.preventDefault();
+    cardRef.current.scrollBy({
+      left: event.deltaY,
+      behavior: "smooth",
+    });
+  };
+
+  useEffect(() => {
+    cardRef.current.addEventListener("wheel", handlewheel);
+  }, []);
+  return (
+    <div className="title-cards">
+      <h2>{title ? title : "Popular on Netflix"}</h2>
+      <div className="card-list" ref={cardRef}>
+        {cards_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default TitleCards;
